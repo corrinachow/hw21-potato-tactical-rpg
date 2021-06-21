420489ea477def71a4be8cca1a1a76599c7bdb49
Jason Rametta <jason.rametta@unity3d.com>
2021-06-21 16:59:44 -0400

    Camera follow player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,13 +4,37 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    void Start()
+    private BoxCollider2D boxCollider;
+    private Vector3 moveDelta;
+    private RaycastHit2D hit;
+
+    private void Start()
     {
-        
+        boxCollider = GetComponent<BoxCollider2D>();
     }
 
-    void Update()
+    private void FixedUpdate()
     {
-        
+        var x = Input.GetAxisRaw("Horizontal");
+        var y = Input.GetAxisRaw("Vertical");
+        moveDelta = new Vector3(x, y, 0);
+
+        // Swap sprite direction when going left or right
+        if (moveDelta.x > 0)
+            transform.localScale = Vector3.one;
+        else if (moveDelta.x < 0)
+            transform.localScale = new Vector3(-1, 1, 0);
+
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(0, moveDelta.y), Mathf.Abs(moveDelta.y * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+        if (hit.collider == null)
+        {
+            transform.Translate(0, moveDelta.y * Time.deltaTime, 0);
+        }
+
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(moveDelta.x, 0), Mathf.Abs(moveDelta.x * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+        if (hit.collider == null)
+        {
+            transform.Translate(moveDelta.x * Time.deltaTime, 0, 0);
+        }
     }
 }
