aea70aa81f2219cf6564e2267aa74d14199a290f
Jason Rametta <jason.rametta@unity3d.com>
2021-06-23 18:17:06 -0400

    Better raycast logic

--- Assets/Scripts/MagicWand.cs
+++ Assets/Scripts/MagicWand.cs
@@ -22,27 +22,19 @@ public class MagicWand : MonoBehaviour
         // A layer mask to get only Blocking elements and targetTag elements
         var layerMask = LayerMask.GetMask(targetTag, "Blocking");
 
-        // Get all the practice targets and send a raycast to each to see if
+        // Get all the targets and send a raycast to each to see if
         // they are in line-of-sight. If they are, shoot a real fireball
         var practiceTargets = GameObject.FindGameObjectsWithTag(targetTag);
 
         foreach (var practiceTarget in practiceTargets)
         {
             var direction = (practiceTarget.transform.position - firePoint.position).normalized;
-            var hits = Physics2D.RaycastAll(firePoint.position, direction * 1000, 1000, layerMask);
+            var hit = Physics2D.Raycast(firePoint.position, direction * 1000, 1000, layerMask);
 
             // debug purposes only
             Debug.DrawRay(firePoint.position, direction * 1000);
 
-            // The raycast indicates all the colliders it hit within the layer mask
-            // so we check the index of the hits, if it hits a Blocking item before it
-            // hits our target, then we know it's not in sight. If it hits our target
-            // before it hits a wall or something blocking, we know it's in sight.
-            var indexOfBlocking = Array.FindIndex(hits, hit => LayerMask.LayerToName(hit.transform.gameObject.layer) == "Blocking");
-            var indexOfTarget = Array.FindIndex(hits, hit => LayerMask.LayerToName(hit.transform.gameObject.layer) == targetTag);
-
-            // We check for -1 because if no blocking elements were found, that is good too
-            if (indexOfTarget < indexOfBlocking || indexOfBlocking == -1)
+            if (hit.transform.CompareTag(targetTag))
             {
                 GameObject go = Instantiate(bulletPrefab, firePoint.position, Quaternion.identity) as GameObject;
                 go.SendMessage("Fire", direction);
