815b25a911b957b07f3b0fcfd7cee50960a3a7c7
Jason Rametta <jason.rametta@unity3d.com>
2021-06-24 13:36:53 -0400

    use target layers

--- Assets/Scripts/MagicWand.cs
+++ Assets/Scripts/MagicWand.cs
@@ -7,7 +7,8 @@ public class MagicWand : MonoBehaviour
 {
     public Transform firePoint;
     public GameObject bulletPrefab;
-    public string targetTag = "PracticeTarget";
+    public string targetTag = "RedTeam";
+    public string targetLayer = "RedTarget";
 
     void Update()
     {
@@ -20,16 +21,22 @@ public class MagicWand : MonoBehaviour
     void Shoot ()
     {
         // A layer mask to get only Blocking elements and targetTag elements
-        var layerMask = LayerMask.GetMask(targetTag, "Blocking");
+        var layerMask = LayerMask.GetMask(targetLayer, "Blocking");
 
         // Get all the targets and send a raycast to each to see if
         // they are in line-of-sight. If they are, shoot a real fireball
-        var practiceTargets = GameObject.FindGameObjectsWithTag(targetTag);
+        var targets = GameObject.FindGameObjectsWithTag(targetTag);
 
-        foreach (var practiceTarget in practiceTargets)
+        Debug.Log("Targets: " + targets.Length);
+        Debug.Log(targetTag);
+
+        foreach (var target in targets)
         {
-            var direction = (practiceTarget.transform.position - firePoint.position).normalized;
+            var direction = (target.transform.position - firePoint.position).normalized;
             var hit = Physics2D.Raycast(firePoint.position, direction * 1000, 1000, layerMask);
+
+            Debug.DrawRay(firePoint.position, direction * 10_000, Color.red, 10f);
+            Debug.Log("Tag hit: " + hit.transform.tag + " | Name: " + hit.transform.name);
 
             if (hit.transform.CompareTag(targetTag))
             {
